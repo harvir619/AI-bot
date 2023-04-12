392b567c0f96229ea5fdf1effb8f96c0f966410f
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-04-12 14:28:40 +0800

    Create App_20230328175218.jsx

---
+++ .history/src/App_20230328175218.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container,Box } from '@chakra-ui/react'
+const App = () => {
+  return (
+    <Box bg='blue.600'>
+    <h1>My App</h1>
+    </Box>
+  )
+}
+
+export default App
