5ec6a0e5248f1065f5e185c7270069df17b13f9a
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-04-14 17:54:28 +0800

    complete ai extractor with modal

---
+++ .history/src/App_20230414172649.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Container, Box } from '@chakra-ui/react'
+import { useState } from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TextInput from './components/TextInput'
+import KeywordsModal from './components/KeywordsModal';
+
+const App = () => {
+  const [keywords, setKeywords] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+  
+  const extractKeywords = async (text) => {
+    setLoading(true)
+    setIsOpen(true)
+    
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and seperate with commas\n\n' + text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8
+      })
+    }
+    
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+    
+    const json = await response.json();
+    
+    const data = json.choices[0].text.trim();
+    
+    console.log(data);
+    setKeywords(data);
+    setLoading(false);
+    
+
+    
+  };
+      const closeModal = () => {
+      setIsOpen(false)
+    }
+  return ( 
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+      <Container maxW='3xl' centerContent>
+        <Header />
+        <TextInput extractKeywords={ extractKeywords} />
+      <Footer/>
+      </Container>
+      <KeywordsModal keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal} />
+    </Box>
+  )
+}
+
+export default App
---
+++ .history/src/components/KeywordsModal_20230414170023.jsx
@@ -0,0 +1,41 @@
+import {
+    Text,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    CircularProgress
+} from '@chakra-ui/react'
+
+const KeywordsModal = ({keywords,loading,isOpen,closeModal}) => {
+  return (
+      <>
+          <Modal isOpen={isOpen} onClose={closeModal}>
+                  
+              <ModalOverlay />
+              <ModalContent>
+                  <ModalHeader>
+                      Keywords
+                  </ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody display='flex' alignItems='center' justifyContent='center'>
+                      {loading ? (
+                          <CircularProgress isIndeterminate color='blue.300' />
+                      ) : (
+                              <Text>
+                                  {keywords}
+                              </Text>
+                      )
+                     
+                  </ModalBody>
+              </ModalContent>
+        </Modal>
+      </>
+  )
+}
+
+export default KeywordsModal
